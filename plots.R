fcbd710a8073388907be943c891a74274ea27467
wzmli <wzmichael.li@gmail.com>
2016-05-25 09:29:48 -0400

    plots code

--- plots.R
+++ plots.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(reshape2)
+scale_colour_discrete <- function(...,palette="Dark2")
+  scale_colour_brewer(...,palette=palette)
 
 names <- list.files(pattern = "RDS")
 dat <- data.frame()
@@ -29,7 +31,7 @@ dat2 <- (dat
   %>% unite(t_p_o_par_plat,type,process,observation,par,platform) 
   %>% ungroup()
   %>% group_by(sample,p,o,parameter)
-  %>% mutate(Timing=timing/max(timing,na.rm=TRUE))
+#  %>% mutate(Timing=timing/max(timing,na.rm=TRUE))
   %>% filter(parameter!="Effprop*rep")
   %>% ungroup()
   %>% group_by(p,o,parameter,t,plat)
@@ -38,11 +40,14 @@ dat2 <- (dat
              )
   %>% ungroup()
 )
+
 sumdat <- (dat2
   %>% group_by(t_p_o_par_plat)
   %>% summarise(Mean = mean(mean)
-                , Timing = mean(Timing)
+                , timing = mean(timing)
                 , coverage=mean(CI95_low<true_parameter & true_parameter<CI95_upp)
+                , lowerCI = mean(CI95_low)
+                , upperCI = mean(CI95_upp)
                 , type = first(t)
                 , process = first(p)
                 , observation = first(o)
@@ -51,30 +56,38 @@ sumdat <- (dat2
   )
 )
 
-g1 <- (ggplot(dat2,aes(x=t_p_o_par_plat,y=mean,color=o))
-  + geom_boxplot()
-  + facet_grid(parameter~t+p,scale="free")
-  # + geom_line(aes(group=sample),alpha=0.5)
-  + theme_bw()
-)
-
-sumdat2 <- melt(sumdat,id=c("t_p_o_par_plat"
+msumdat <- melt(sumdat,id=c("t_p_o_par_plat"
+                            , "timing"
                             , "type"
                             , "process"
                             , "observation"
                             , "platform"
                             , "parameter"
-                            , "Mean"))
+                            , "coverage"))
 
-g2 <- (ggplot(subset(sumdat2,parameter=="R0"),aes(x=platform,y=value,color=variable))
+g1 <- (ggplot(msumdat,aes(x=t_p_o_par_plat,y=value,color=observation,group=t_p_o_par_plat))
   + geom_point()
-  + geom_line(aes(group=variable))
-  + facet_grid(observation~type+process,scale="free")
+  + geom_line()
+  + facet_grid(parameter~type+process,scale="free",space = "free_x")
+  # + geom_line(aes(group=sample),alpha=0.5)
   + theme_bw()
+#  + ggtitle("")
+)
+
+msumdat2 <- melt(sumdat,id=c("t_p_o_par_plat"
+                            , "type"
+                            , "process"
+                            , "observation"
+                            , "platform"
+                            , "parameter"
+                            , "Mean"
+                            , "lowerCI"
+                            , "upperCI"))
+
+g2 <- (ggplot(msumdat2,aes(x=platform,y=value,color=parameter,group=platform))
+  + geom_point(size=2)
+  + facet_grid(variable+observation~type+process,scale="free")
+  + theme_bw()
+  + ggtitle("Performance")
   )
 
-g1 %+% subset(sumdat,parameter=="R0")+ geom_hline(yintercept = 2.5)
-
-g1 %+% subset(sumdat,parameter=="effprop")+ geom_hline(yintercept = 0.75)
-
-ggplot(sumdat,aes(x=t_p_o_par,y=Timing)) + geom_point()
