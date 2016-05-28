62649433ddfd701d6898fb168443ec235d09d12d
mli2830 <wzmichael.li@gmail.com>
2016-05-28 17:56:37 -0400

    NB complete

--- stantemplate.R
+++ stantemplate.R
@@ -1,3 +1,5 @@
+if(type[2] == "P"){
+
 cat("I = c(",niminits$I[1],sub("",",",niminits$I[-1]),")"
     , "\n" ,"effprop = ",effprop
     , "\n" , "R0 =", R0
@@ -19,6 +21,7 @@ cat("data {
 int<lower=0> numobs; // number of data points
     int obs[numobs]; // response
     int N;
+    int i0;
     real eps;
     }
     parameters {
@@ -28,7 +31,6 @@ int<lower=0> numobs; // number of data points
     real <lower=0> I[numobs];
     real  <lower=0> N0;
     real <lower=0,upper=N> Nmean;
-    real <lower=0> i0;
     real <lower=0> Ndis;
     }
     model {
@@ -37,11 +39,11 @@ int<lower=0> numobs; // number of data points
     vector[numobs] Imean;
     real BETA;
     Ndis ~ uniform(0,100)
+    effprop ~ beta(100,35);
+    repMean ~ beta(70,100);
     Nmean ~ gamma(fmax(Ndis,eps),Ndis/(effprop*N));
     N0 ~ gamma(Nmean,1);
     BETA <- exp(-R0/N0);
-    effprop ~ beta(100,35);
-    repMean ~ beta(70,100);
     Imean[1] <- fmax((1-BETA)*N0,eps);
     I[1] ~ gamma(Imean[1],1);
     S[1] <- N0 - I[1];
@@ -60,3 +62,71 @@ int<lower=0> numobs; // number of data points
 )
 
 stanmod = paste(type[2],type[3],seed,"stan",sep=".")
+}
+
+if(type[2] == "NB"){
+  
+  cat("I = c(",niminits$I[1],sub("",",",niminits$I[-1]),")"
+      , "\n" ,"effprop = ",effprop
+      , "\n" , "R0 =", R0
+      , "\n" , "repMean = ", repMean
+      , "\n" , "Nmean = ", Nmean
+      , file = paste(type[2],type[3],seed,"init.R",sep=".")
+  )
+  
+  cat("obs = c(",sim$Iobs[1],sub("",",",sim$Iobs[-1]),")"
+      , "\n" ,"N = ",N
+      , "\n" , "numobs =", numobs
+      , "\n" , "i0 = ", i0
+      , "\n" , "eps = ", eps
+      , file = paste(type[2],type[3],seed,"data.R",sep=".")
+  )
+  
+  #NB process
+  cat("data {
+      int<lower=0> numobs; // number of data points
+      int obs[numobs]; // response
+      int N;
+      int i0;
+      real eps;
+}
+parameters {
+real <lower=0.01,upper=4> R0;
+real <lower=0.1,upper=0.5> repMean;
+real <lower=0.5,upper=0.9> effprop;
+real <lower=0> I[numobs];
+real  <lower=0> N0;
+real <lower=0,upper=N> Nmean;
+real <lower=0> Ndis;
+real <lower=0> Pdis;
+}
+model {
+vector[numobs] S;
+vector[numobs-1] pSI;
+vector[numobs] Imean;
+real BETA;
+Ndis ~ uniform(0.1,100);
+effprop ~ beta(100,35);
+repMean ~ beta(70,100);
+Pdis ~ uniform(0.1,100);
+Nmean ~ gamma(fmax(Ndis,eps),Ndis/(effprop*N));
+N0 ~ gamma(Nmean,1);
+
+I[1] ~ gamma(i0,1);
+BETA <- exp(-R0/N0);
+S[1] <- N0 - I[1];
+obs[1] ~ poisson(repMean*I[1]);
+
+
+for (t in 2:numobs) {
+pSI[t-1] <- 1 - BETA^I[t-1];
+I[t] ~ gamma(fmax(Pdis,eps),fmax(Pdis/(pSI[t-1]*S[t-1]),eps));;
+S[t] <- S[t-1] - I[t];
+obs[t] ~ poisson(repMean*I[t]);
+}
+}"
+    , file = paste(type[2],type[3],seed,"stan",sep=".")
+)
+
+stanmod = paste(type[2],type[3],seed,"stan",sep=".")
+}
