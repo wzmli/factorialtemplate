9257d2dd1f633ad18cf74deb65036ca15780c1ff
wzmli <wzmichael.li@gmail.com>
2016-06-01 12:49:04 -0400

    updating plots

--- plots.R
+++ plots.R
@@ -44,10 +44,13 @@ dat2 <- (dat
 sumdat <- (dat2
   %>% group_by(t_p_o_par_plat)
   %>% summarise(Mean = mean(mean)
-                , timing = mean(timing)
+                , Median = median(median)
+                , timing = median(timing)
                 , coverage=mean(CI95_low<true_parameter & true_parameter<CI95_upp)
-                , lowerCI = mean(CI95_low)
-                , upperCI = mean(CI95_upp)
+                , ESS = median(ess)
+                , efficiency = median(efficiency) 
+                , lowerCI = median(CI95_low)
+                , upperCI = median(CI95_upp)
                 , type = first(t)
                 , process = first(p)
                 , observation = first(o)
@@ -63,7 +66,9 @@ msumdat <- melt(sumdat,id=c("t_p_o_par_plat"
                             , "observation"
                             , "platform"
                             , "parameter"
-                            , "coverage"))
+                            , "coverage"
+                            , "ESS"
+                            , "efficiency"))
 
 g1 <- (ggplot(msumdat,aes(x=t_p_o_par_plat,y=value,color=observation,group=t_p_o_par_plat))
   + geom_point()
@@ -71,8 +76,11 @@ g1 <- (ggplot(msumdat,aes(x=t_p_o_par_plat,y=value,color=observation,group=t_p_o
   + facet_grid(parameter~type+process,scale="free",space = "free_x")
   # + geom_line(aes(group=sample),alpha=0.5)
   + theme_bw()
-#  + ggtitle("")
+  + ggtitle("Parameter Estimates")
 )
+
+hline.data <- data.frame(z = c(0.7,2,0.4), parameter = c("effprop","R0","repMean"))
+g1 + geom_hline(aes(yintercept = z), hline.data)
 
 msumdat2 <- melt(sumdat,id=c("t_p_o_par_plat"
                             , "type"
@@ -91,3 +99,12 @@ g2 <- (ggplot(msumdat2,aes(x=platform,y=value,color=parameter,group=platform))
   + ggtitle("Performance")
   )
 
+
+g3 <- (ggplot(msumdat,aes(x=timing,y=coverage,color=platform,group=platform,shape=parameter))
+       + geom_point(size=4)
+       + facet_grid(type~process)
+       + theme_bw()
+       + ggtitle("Performance")
+)
+
+g3
--- plotting_sim.R
+++ plotting_sim.R
@@ -1,7 +1,33 @@
 ## plotting simulated data
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(tidyr)
 
-seed = 1:200
-sims = lapply(seed,simm,R0=R0,N=N,
-              effprop=effprop,i0=i0,repMean=repMean,repSize=repSize,numobs=numobs)
-matplot(sapply(sims,"[[","Iobs"),type="l",lty=1)
+seed = 1:100
+simdat <- data.frame()
+for(i in 1:50){
+  tempdat <- data.frame(seed=i,simm(R0=R0,N=N,effprop=effprop,i0=i0,repMean=repMean,
+                             repSize=repSize,numobs=numobs,seed=i))
+  simdat <- rbind(simdat,tempdat)
+}
+
+simdf <- (simdat 
+  %>% select(c(seed,time,I,Iobs))
+)
+
+msimdf <- melt(simdf,id.vars = c("seed","time"))
+
+msimdf <- msimdf %>% mutate(t=variable,s=seed) %>% unite(s_t,s,t)
+
+msimdf$variable <- factor(msimdf$variable,levels = c("Iobs","I"))
+
+(ggplot(msimdf,aes(x=time,y=value,group=s_t,color=factor(seed),linetype=variable))
+  + geom_line()
+  + theme(legend.position="none")
+  + ylab("Number of Infectives")
+  + xlab("Time")
+)
+
+
 
